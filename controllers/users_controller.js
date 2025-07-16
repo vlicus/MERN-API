fb41a2f01fdb573184e3d796bdd6eb0a87773b54
Samuel Cobas <samu.cobas.ort@gmail.com>
2025-07-16 20:26:10 +0200

    feat: manual autentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -15,20 +15,34 @@ usersRouter.get('/', async (req, res) => {
 
 // POST
 
-usersRouter.post('/', async (req, res) => {
+usersRouter.post('/', async (req, res, next) => {
     const { username, name, password } = req.body;
 
-    const passwordHash = await bcrypt.hash(password, 10);
+    if (!username || !password) {
+        return res.status(400).json({
+            error: 'username and password are required',
+        });
+    }
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({
+                error: 'user already exists',
+            });
+        }
+        const passwordHash = await bcrypt.hash(password, 10);
 
-    const user = new User({
-        username,
-        name,
-        passwordHash,
-    });
+        const user = new User({
+            username,
+            name,
+            passwordHash,
+        });
 
-    const savedUser = await user.save();
-
-    res.status(201).json(savedUser);
+        const savedUser = await user.save();
+        return res.status(201).json(savedUser);
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = usersRouter;
